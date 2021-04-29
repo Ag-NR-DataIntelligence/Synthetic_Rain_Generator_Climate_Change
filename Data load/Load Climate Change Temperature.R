727a2aeb7c0b33505100840360a1bf5c7227be1e
Ziwen Yu <ziwenyu@ssrb-vpn1-4-43.vpn.ufl.edu>
2021-04-29 09:37:26 -0400

    Add hisotrical monthly temperature

--- Data load/Load Climate Change Temperature.R
+++ Data load/Load Climate Change Temperature.R
@@ -6,11 +6,22 @@ file="https://www.dropbox.com/s/slcv2huiovx8ary/BOS%20%20Temperature.csv?dl=1"
 BOS_MonthT <- read_csv(file) %>% mutate(Loc="BOS")
 file="https://www.dropbox.com/s/q5izlalq7lag7hh/NYC%20%20Temperature.csv?dl=1"
 NYC_MonthT <- read_csv(file) %>% mutate(Loc="NYC")
+Raw_dt %>% 
+  group_by(Loc,Yr=year(Time),Mon=month(Time))  %>% 
+  summarise(MonT=mean(Temp,na.rm=T)) %>% 
+  filter(!is.na(MonT)) %>% 
+  ungroup %>% 
+  mutate(Date=ymd(glue("{Yr}-","{Mon}","-01")),
+         Emission=NA,
+         Model="History") %>% 
+  select(Date,Emission,Loc,Model,MonT)->His_MonthT
 
 MonthT_all=bind_rows(PHL_MonthT,BOS_MonthT,NYC_MonthT) %>% 
   mutate(Date=ymd(Date)) %>% 
-  gather(.,Model,MonT,-Date,-Emission,-Loc)
+  gather(.,Model,MonT,-Date,-Emission,-Loc) %>% 
+  bind_rows(His_MonthT)
 
 rm(BOS_MonthT)
 rm(PHL_MonthT)
 rm(NYC_MonthT)
+rm(His_MonthT)
