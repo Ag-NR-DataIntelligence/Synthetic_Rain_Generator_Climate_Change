4c8629a03e51c783b62c552106ce1460a4046eb9
Ziwen Yu <ziwenyu@Ziwens-iMac.attlocal.net>
2021-04-29 08:04:14 -0400

    Updated the source targets

--- Code to create and save the synthetic data.R
+++ Code to create and save the synthetic data.R
@@ -1,5 +1,8 @@
+source("https://raw.githubusercontent.com/Ag-NR-DataIntelligence/Synthetic_Rain_Generator_Climate_Change/master/Data%20load/Processing%20Functions.R")
+source("https://raw.githubusercontent.com/Ag-NR-DataIntelligence/Synthetic_Rain_Generator_Climate_Change/master/Data%20load/Load%20Raw%20Data.R")
+source("https://raw.githubusercontent.com/Ag-NR-DataIntelligence/Synthetic_Rain_Generator_Climate_Change/master/Data%20load/Load%20Climate%20Change%20Temperature.R")
 source('https://raw.githubusercontent.com/ZyuAFD/SWRE_Project/master/Non_stationaryGenerator/Non_StationaryInterMFunctions.R')
-source('https://raw.githubusercontent.com/ZyuAFD/SWRE_Project/master/Non_stationaryGenerator/Generator.R')
+source('https://raw.githubusercontent.com/Ag-NR-DataIntelligence/Synthetic_Rain_Generator_Climate_Change/master/Algorithm/Generator%20Functions.R')
 
 
 MonthT=MonthT_all %>% filter(Loc=="NYC",Emission=="A2")
